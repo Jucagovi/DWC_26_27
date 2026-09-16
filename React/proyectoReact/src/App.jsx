6486c0c6b6c0a305476c7901a64c688cacd77f1a
Juan Carlos Gómez Vicente <jc.gomez@edu.gva.es>
2026-09-16 16:33:30 +0200

    Creación de proyectos React y Vanilla

---
+++ React/proyectoReact/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <section id='center'>
+        <h2>¡Hola, React!</h2>
+      </section>
+    </>
+  );
+}
+
+export default App;
